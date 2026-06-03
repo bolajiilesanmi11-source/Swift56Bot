087a1b784b01e499a84c93e426da7ea4d04dc78e
bolajiilesanmi11-source <bolajiilesanmi11@gmail.com>
2026-06-04 00:15:25 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 # This decorator listens for the /start command
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    welcome_text = "I passed my challenge within just 6 days using this EA 🔥📊
-"
+    welcome_text = "💬 Join discussions, get updates, and connect with other"
     bot.reply_to(message, welcome_text)
 
 # This keeps the bot running and listening for messages
